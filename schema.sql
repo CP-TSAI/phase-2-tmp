fb5cc7e4aaf12e92945119504b20fb1f65b6a522
cptsai <aupt23@gmail.com>
2022-07-01 15:05:59 -0400

    added UNIQUE constraint

--- schema.sql
+++ schema.sql
@@ -8,9 +8,10 @@
 -- Foreign Keys: FK_ChildTable_childColumn_ParentTable_parentColumn     
 --    Example: fk_AdminUser_email_User_email
 
+
 -- ###################### Notes ######################
 -- Note: Change the table name to the naming convention (TODO: the TABLE in report doesn't 100% follow the naming convention)
--- TODO: should also test the code in sql (ex: SQLite) and make sure it runs
+-- run the file with SQLite: `sqlite3 < schema.sql`
 
 CREATE TABLE "User" (
   "email" varchar(250) NOT NULL,
@@ -20,28 +21,33 @@ CREATE TABLE "User" (
   "nickname" varchar(250) NOT NULL,
   "postal_code" varchar(250) NOT NULL,
   PRIMARY KEY ("email"),
+  UNIQUE("email"),
   FOREIGN KEY ("postal_code") REFERENCES "LocationLookup" ("postal_code")
 );
 
 CREATE TABLE "Item" (
   "lister_email" varchar(250) NOT NULL,
   "title" varchar(250) NOT NULL,
-  "item_no" int(16) NOT NULL,
+  "item_no" int(16) NOT NULL, -- Note: in SQLite an int primary key will autoincrement by itself, so no need to use "AUTO_INCREMENT"
   "game_type" varchar(250) NOT NULL,
   "number_of_cards" int(16) NULL,
   "platform" varchar(250) NULL,
   "media" varchar(250) NULL,
   "condition" varchar(250) NOT NULL,
-  "description" varchar(250) NULL,
+  "description" varchar(250) DEFAULT NULL,
   "listing_url" varchar(250) NOT NULL,
-  PRIMARY KEY ("item_no")
+  PRIMARY KEY ("item_no"),
+  UNIQUE("item_no"),
+  FOREIGN KEY ("lister_email") REFERENCES "User" ("email"),
+  FOREIGN KEY ("game_type") REFERENCES "GamePlatformMap" ("game_type")
 );
 
 
 CREATE TABLE "GamePlatformMap" (
   "game_type" varchar(250) NOT NULL,
   "platform" varchar(250) NOT NULL,
-  PRIMARY KEY ("game_type")
+  PRIMARY KEY ("game_type"),
+  UNIQUE("game_type")
 );
 
 CREATE TABLE "Trade" (
@@ -50,10 +56,11 @@ CREATE TABLE "Trade" (
   "proposer_item_no" int(16) NOT NULL,
   "counterparty_item_no" int(16) NOT NULL,
   "proposed_date" datetime NOT NULL,
-  "accept_reject_date" datetime NULL,
+  "accept_reject_date" datetime DEFAULT NULL,
   "status" varchar(250) NOT NULL,
   "trade_history_link" varchar(250) NOT NULL,
-  PRIMARY KEY ("proposer_email","counterparty_email","proposer_item_no","counterparty_item_no"),
+  "auto_trade_id" int(16) NOT NULL, -- Note: in SQLite an int primary key will autoincrement by itself, so no need to use "AUTO_INCREMENT"
+  PRIMARY KEY ("auto_trade_id"),
   FOREIGN KEY ("proposer_email") REFERENCES "User" ("email"),
   FOREIGN KEY ("counterparty_email") REFERENCES "User" ("email"),
   FOREIGN KEY ("proposer_item_no") REFERENCES "Item" ("item_no"),
@@ -66,10 +73,10 @@ CREATE TABLE "LocationLookup" (
   "state" varchar(250) NOT NULL,
   "latitude" float(8) NOT NULL,
   "longitude" float(8) NOT NULL,
-  PRIMARY KEY ("postal_code")
+  PRIMARY KEY ("postal_code"),
+  UNIQUE("postal_code")
 );
 
--- TODO: do we need a primary key here?
 CREATE TABLE "ResponseColorLookup" (
   "response_lower_range" float(8) NOT NULL,
   "response_upper_range" float(8) NOT NULL,
