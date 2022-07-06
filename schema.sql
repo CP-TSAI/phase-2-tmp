b310f4c0115a1094766eb8cc5cdec9ed428cc01d
cagim3 <chisomaagim@gmail.com>
2022-07-06 01:22:08 -0400

    Edit 4: added new item tables, edited naming convention, added extra primary key attribute to all item tables

--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
 -- To check with SQLite3, run command: `$ sqlite3 < schema.sql`
 
 CREATE TABLE `User` (
-  Email varchar(250) NOT NULL,
+  email varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
   first_name varchar(250) NOT NULL,
   last_name varchar(250) NOT NULL,
   nickname varchar(250) NOT NULL,
   postal_code varchar(250) NOT NULL,
-  PRIMARY KEY (Email),
+  PRIMARY KEY (email),
   UNIQUE(nickname),
   FOREIGN KEY (postal_code) REFERENCES Location_Lookup (postal_code)
 );
@@ -15,24 +15,84 @@ CREATE TABLE `User` (
 CREATE TABLE Item (
   lister_email varchar(250) NOT NULL,
   title varchar(250) NOT NULL,
-  Item_no int(16) NOT NULL, -- Note: in SQLite an int primary key will autoincrement by itself, so no need to use "AUTO_INCREMENT"
-  game_type varchar(250) NOT NULL,
-  number_of_cards int(16) NULL,
-  platform varchar(250) NULL,
-  media varchar(250) NULL,
+  item_no int(16) NOT NULL, -- Note: in SQLite an int primary key will autoincrement by itself, so no need to use "AUTO_INCREMENT"
+  game_type varchar(250) NOT NULL, --delete?
+  number_of_cards int(16) NULL, --delete?
+  platform varchar(250) NULL, --delete?
+  media varchar(250) NULL, --delete + all the above because of the new separate tables? -JA
   condition varchar(250) NOT NULL,
   description varchar(250) NULL,
   listing_url varchar(250) NOT NULL,
-  PRIMARY KEY (Item_no),
+  PRIMARY KEY (item_no, lister_email),
   FOREIGN KEY (lister_email) REFERENCES `User` (email),
   FOREIGN KEY (game_type) REFERENCES Game_Platform_Map (game_type)
 );
 
+CREATE TABLE Item_Collectable_Card_Game (
+  lister_email varchar(250) NOT NULL,
+  item_no int(16) NOT NULL,
+  number_of_cards int(16) NULL,
+  PRIMARY KEY (item_no, lister_email),
+  FOREIGN KEY (item_no) REFERENCES Item (item_no),
+  FOREIGN KEY (lister_email) REFERENCES `User` (email)
+);
+
+CREATE TABLE Item_Board_Game (
+  lister_email varchar(250) NOT NULL,
+  item_no int(16) NOT NULL,
+  PRIMARY KEY (item_no, lister_email),
+  FOREIGN KEY (item_no) REFERENCES Item (item_no),
+  FOREIGN KEY (lister_email) REFERENCES `User` (email)
+);
+
+CREATE TABLE Item_Playing_Card_Game (
+  lister_email varchar(250) NOT NULL,
+  item_no int(16) NOT NULL,
+  PRIMARY KEY (item_no, lister_email),
+  FOREIGN KEY (item_no) REFERENCES Item (item_no),
+  FOREIGN KEY (lister_email) REFERENCES `User` (email)
+);
+
+CREATE TABLE Item_Computer_Game (
+  lister_email varchar(250) NOT NULL,
+  item_no int(16) NOT NULL,
+  media varchar(250) NULL,
+  PRIMARY KEY (item_no, lister_email),
+  FOREIGN KEY (item_no) REFERENCES Item (item_no),
+  FOREIGN KEY (lister_email) REFERENCES `User` (email)
+);
+
+CREATE TABLE Item_Video_Game (
+  lister_email varchar(250) NOT NULL,
+  item_no int(16) NOT NULL,
+  platform varchar(250) NULL,
+  media varchar(250) NULL,
+  PRIMARY KEY (item_no, lister_email, platform),
+  FOREIGN KEY (item_no) REFERENCES Item (item_no),
+  FOREIGN KEY (lister_email) REFERENCES `User` (email)
+  FOREIGN KEY (platform) REFERENCES Platform (`name`)
+);
+
+CREATE TABLE Item_Desired (
+  lister_email varchar(250) NOT NULL,
+  item_no int(16) NOT NULL,
+  PRIMARY KEY (item_no, lister_email),
+  FOREIGN KEY (item_no) REFERENCES Item (item_no),
+  FOREIGN KEY (lister_email) REFERENCES `User` (email)
+);
+
+CREATE TABLE Item_Proposed (
+  lister_email varchar(250) NOT NULL,
+  item_no int(16) NOT NULL,
+  PRIMARY KEY (item_no, lister_email),
+  FOREIGN KEY (item_no) REFERENCES Item (item_no),
+  FOREIGN KEY (lister_email) REFERENCES `User` (email)
+);
 
 CREATE TABLE Game_Platform_Map (
-  Game_type varchar(250) NOT NULL,
+  game_type varchar(250) NOT NULL,
   platform varchar(250) NOT NULL,
-  PRIMARY KEY (Game_type)
+  PRIMARY KEY (game_type)
 );
 
 CREATE TABLE Trade (
@@ -44,8 +104,8 @@ CREATE TABLE Trade (
   accept_reject_date datetime NULL,
   status varchar(250) NOT NULL,
   trade_history_link varchar(250) NOT NULL,
-  Auto_trade_id int(16) NOT NULL, -- Note: in SQLite an int primary key will autoincrement by itself, so no need to use "AUTO_INCREMENT"
-  PRIMARY KEY (Auto_trade_id),
+  auto_trade_id int(16) NOT NULL, -- Note: in SQLite an int primary key will autoincrement by itself, so no need to use "AUTO_INCREMENT"
+  PRIMARY KEY (auto_trade_id),
   FOREIGN KEY (proposer_email) REFERENCES `User` (email),
   FOREIGN KEY (counterparty_email) REFERENCES `User` (email),
   FOREIGN KEY (proposer_item_no) REFERENCES Item (item_no),
@@ -53,12 +113,12 @@ CREATE TABLE Trade (
 );
 
 CREATE TABLE Location_Lookup (
-  Postal_code varchar(250) NOT NULL,
+  postal_code varchar(250) NOT NULL,
   city varchar(250) NOT NULL,
   state varchar(250) NOT NULL,
   latitude float(8) NOT NULL,
   longitude float(8) NOT NULL,
-  PRIMARY KEY (Postal_code)
+  PRIMARY KEY (postal_code)
 );
 
 CREATE TABLE Distance_color_lookup (
@@ -69,16 +129,16 @@ CREATE TABLE Distance_color_lookup (
 );
 
 CREATE TABLE Response_color_lookup (
-  Response_lower_range float(8) NOT NULL,
-  Response_upper_range float(8) NOT NULL,
+  response_lower_range float(8) NOT NULL,
+  response_upper_range float(8) NOT NULL,
   text_color varchar(250) NOT NULL,
-  PRIMARY KEY (Response_lower_range, Response_upper_range)
+  PRIMARY KEY (response_lower_range, response_upper_range)
 );
 
 
 CREATE TABLE Rank_lookup (
-  Trade_lower_range int(16) NOT NULL,
-  Trade_upper_range int(16) NOT NULL,
+  trade_lower_range int(16) NOT NULL,
+  trade_upper_range int(16) NOT NULL,
   rank_label varchar(250) NOT NULL,
-  PRIMARY KEY (Trade_lower_range, Trade_upper_range)
+  PRIMARY KEY (trade_lower_range, trade_upper_range)
 );
